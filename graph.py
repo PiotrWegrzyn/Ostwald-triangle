2438f3ba0331ea9aca8d2f2221176fca9218ed06
Peter <peterw1114@gmail.com>
2019-07-06 19:57:56 +0200

    Resize window

--- graph.py
+++ graph.py
@@ -1,13 +1,16 @@
 from geometry.line import Line
+from geometry.point import Point
 
 
 class Graph:
 
     def __init__(self):
-        self.top = 600
-        self.bot = 200
+        self.top = 650
+        self.bot = 350
         self.height = self.top - self.bot
         self.left = 100
         self.right = 500
         self.width = self.right - self.left
-        self.line_co2 = Line(self.left, self.bot, self.left, self.top)
+        self.line_co2 = Line(self.left, self.top, self.left, self.bot)
+        self.line_co = Line(Point(self.right, self.bot), 360-(90+45), self.height)
+        self.line_o2 = Line(self.left, self.bot, self.right, self.bot)
