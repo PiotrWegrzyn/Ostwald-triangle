01c7bd42a6ded471474b6afa4068eb994a653cbb
Peter <peterw1114@gmail.com>
2019-07-08 01:19:35 +0200

    add split points poligonalchain

--- geometry/__init__.py
+++ geometry/__init__.py
@@ -1,2 +1,3 @@
 from .point import Point
 from .vector import Vector
+from .poligonalchain import PolygonalChain
--- geometry/tests.py
+++ geometry/tests.py
@@ -1,6 +1,7 @@
 import unittest
 from math import atan, degrees
 
+from geometry import PolygonalChain
 from geometry.vector import Vector
 from geometry.point import Point
 
@@ -84,7 +85,7 @@ class TestVectorClass(unittest.TestCase):
         a = Point(0, 0)
         b = Point(10, 10)
         vector = Vector(a, b)
-        middle = vector.get_middle()
+        middle = vector.get_geometric_center()
         self.assertEqual(Point(5, 5), middle)
 
     def test_split_correct_amount_of_vectors(self):
@@ -125,13 +126,80 @@ class TestVectorClass(unittest.TestCase):
         vector_reversed = Vector(b, a)
         self.assertEqual(vector_reversed, vector.reversed())
 
-    def test_connect(self):
+
+class TestPolygonalChainClass(unittest.TestCase):
+    def test_init_start_point(self):
         a = Point(0, 0)
         b = Point(10, 10)
         c = Point(20, 0)
         vector1 = Vector(a, b)
         vector2 = Vector(b, c)
-        self.assertEqual(vector_reversed, vector.reversed())
+        pc = PolygonalChain([vector1,vector2])
+        self.assertEqual(pc.start, a)
+
+    def test_init_end_point(self):
+        a = Point(0, 0)
+        b = Point(10, 10)
+        c = Point(20, 0)
+        vector1 = Vector(a, b)
+        vector2 = Vector(b, c)
+        pc = PolygonalChain([vector1,vector2])
+        self.assertEqual(pc.end, c)
+
+    def test_init_length(self):
+        a = Point(0, 0)
+        b = Point(3, 4)
+        c = Point(6, 0)
+        vector1 = Vector(a, b)
+        vector2 = Vector(b, c)
+        pc = PolygonalChain([vector1, vector2])
+        self.assertEqual(pc.length, 10)
+
+    def test_init_dx(self):
+        a = Point(0, 0)
+        b = Point(3, 4)
+        c = Point(6, 0)
+        vector1 = Vector(a, b)
+        vector2 = Vector(b, c)
+        pc = PolygonalChain([vector1, vector2])
+        self.assertEqual(pc.dx, 6)
+
+    def test_init_dy(self):
+        a = Point(0, 0)
+        b = Point(3, 4)
+        c = Point(6, 0)
+        vector1 = Vector(a, b)
+        vector2 = Vector(b, c)
+        pc = PolygonalChain([vector1, vector2])
+        self.assertEqual(pc.dy, 0)
+
+
+    def test_equally_split_points(self):
+        a = Point(0, 0)
+        b = Point(3, 4)
+        c = Point(6, 0)
+        vector1 = Vector(a, b)
+        vector2 = Vector(b, c)
+        pc = PolygonalChain([vector1, vector2])
+        self.assertEqual(pc.get_split_points(3), [a, b, c])
+
+    def test_equally_split_points2(self):
+        a = Point(0, 0)
+        b = Point(0, 10)
+        c = Point(10, 10)
+        vector1 = Vector(a, b)
+        vector2 = Vector(b, c)
+        pc = PolygonalChain([vector1, vector2])
+        self.assertEqual(pc.get_split_points(5), [a, Point(0, 5), b, Point(5, 10), c])
+
+    def test_waged_split_points(self):
+        a = Point(0, 0)
+        b = Point(0, 10)
+        c = Point(10, 10)
+        vector1 = Vector(a, b)
+        vector2 = Vector(b, c)
+        pc = PolygonalChain([vector1, vector2])
+        self.assertEqual(pc.get_split_points(5,[0,0.4,0.5,0.6,1]), [a, Point(0, 8), b, Point(2, 10), c])
 
 
 if __name__ == '__main__':
