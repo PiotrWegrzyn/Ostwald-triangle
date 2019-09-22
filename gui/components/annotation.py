0a8529e7a5934142a67f9af6beb6020d73473321
peter <peterw1114@gmail.com>
2019-09-23 00:00:59 +0200

    Add scaling font based on screen resolution.

--- gui/components/annotation.py
+++ gui/components/annotation.py
@@ -1,4 +1,5 @@
 from kivy.core.text import Label as CoreLabel
+from kivy.core.window import Window
 from kivy.graphics.vertex_instructions import Rectangle
 
 
@@ -7,9 +8,10 @@ class Annotation(Rectangle):
         if kwargs.get("format", None):
             text = self.format(text, kwargs.get("format", None))
         scale = kwargs.get("scale", 1)
-        size = [len(str(text)) * 8 * scale, 16 * scale]
-        offset_x = kwargs.get("offset_x", 0)
-        offset_y = kwargs.get("offset_y", 0)
+        resolution_scaled_size = ((16/3360)*Window.size[0], (32/2100)*Window.size[1])
+        size = [len(str(text)) * resolution_scaled_size[0] * scale, resolution_scaled_size[1] * scale]
+        offset_x = kwargs.get("offset_x", 0) * Window.size[0]/1650
+        offset_y = kwargs.get("offset_y", 0) * Window.size[1]/1050
         pos = (pos[0]-(size[0]/2)+offset_x, pos[1]-(size[1]/2)+offset_y)
         super().__init__(texture=self.get_texture(text), pos=pos, size=size, *args, **kwargs)
 
